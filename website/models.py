80e0ed4576187eef4f1bf6aa534fad1dbcf3c0c4
Yaekob Bonke Bundure <yaekobbundure@gmail.com>
2023-09-13 18:31:17 +0300

    user and blog models

--- website/models.py
+++ website/models.py
@@ -1,7 +1,6 @@
 from website import db
 from datetime import datetime
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
     """user model"""
@@ -11,7 +10,6 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), unique=True,nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=True)
-   # password_hash = db.Column(db.String(255), nullable=False)
 
     def __init__(self, username, email, password):
         """constructor of user class"""
@@ -19,11 +17,6 @@ class User(db.Model, UserMixin):
         self.email = email
         self.password = password
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 class Blog(db.Model):
     """creates blog model to create blog post"""
@@ -37,7 +30,3 @@ class Blog(db.Model):
     def __init__(self, title, content):
         self.title = title
         self.content = content
-
-#with app.app_context():
-    #"""creates tables within application context"""
-   # db.create_all()
